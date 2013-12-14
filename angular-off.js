318374a17bb332d352ee28477fb87031d5170f31
PatrickJS <github@gdi2290.com>
2013-12-13 22:28:22 -0800

    update source code to use decorator for defining $off on $rootScope

--- angular-off.js
+++ angular-off.js
@@ -1,15 +1,30 @@
-(function () {
-  angular.injector(['ng']).injector.get('$rootScope')
-      .constructor.prototype.$off = function(eventName, fn) {
-        if(this.$$listeners) {
+;(function(module, angular, undefined) {
+'use strict';
+
+angular.module('ngOff', ['angular-off']);
+
+/* Config */
+module.config(['$provide', function($provide) {
+  $provide.decorator('$rootScope', ['$delegate', function($delegate) {
+
+    Object.defineProperty($delegate.constructor.prototype, '$off', {
+      value: function(eventName, fn) {
+        if (this.$$listeners) {
           if (arguments.length > 1) {
             var namedListeners = this.$$listeners[eventName];
-            if(namedListeners) {
+            if (namedListeners) {
               namedListeners.splice(namedListeners.indexOf(fn), 1);
             }
           } else {
             this.$$listeners[eventName] = null;
           }
         } // end if $$listeners
-      }; //end $off
-}());
+      }, // end value
+      enumerable: false
+    });
+
+    return $delegate;
+  }]);
+}]);
+
+}(angular.module('angular-off', []), angular));
