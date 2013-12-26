383c430b37e2097eaaf546423bbfd7752a28e02a
PatrickJS <github@gdi2290.com>
2013-12-26 09:00:49 -0800

    update source code

--- angular-off.js
+++ angular-off.js
@@ -1,30 +1,32 @@
-;(function(module, angular, undefined) {
-'use strict';
+!function(module, angular, undefined) {
+  'use strict';
 
-angular.module('ngOff', ['angular-off']);
+  angular.module('ngOff', ['angular-off']);
 
-/* Config */
-module.config(['$provide', function($provide) {
-  $provide.decorator('$rootScope', ['$delegate', function($delegate) {
+  /* Config */
+  module.config(['$provide', function($provide) {
 
-    Object.defineProperty($delegate.constructor.prototype, '$off', {
-      value: function(eventName, fn) {
-        if (this.$$listeners) {
-          if (arguments.length > 1) {
-            var namedListeners = this.$$listeners[eventName];
-            if (namedListeners) {
-              namedListeners.splice(namedListeners.indexOf(fn), 1);
-            }
-          } else {
-            this.$$listeners[eventName] = null;
+    function ngOff(eventName, fn) {
+      if (this.$$listeners) {
+        if (arguments.length > 1) {
+          var namedListeners = this.$$listeners[eventName];
+          if (namedListeners) {
+            namedListeners.splice(namedListeners.indexOf(fn), 1);
           }
-        } // end if $$listeners
-      }, // end value
-      enumerable: false
-    });
+        } else {
+          this.$$listeners[eventName] = null;
+        } // end args > 1
+      } // end if $$listeners
+    }
 
-    return $delegate;
+    $provide.decorator('$rootScope', ['$delegate', function($delegate) {
+      Object.defineProperty($delegate.constructor.prototype, '$off', {
+        value: ngOff,
+        enumerable: false
+      });
+      return $delegate;
+    }]);
+
   }]);
-}]);
 
-}(angular.module('angular-off', []), angular));
+}(angular.module('angular-off', []), angular);
